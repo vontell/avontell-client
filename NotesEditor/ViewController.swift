48e9ab84e39dac2931f468de4813964d79e633a1
Aaron Vontell <vontell@mit.edu>
2016-01-19 14:22:33 -0500

    Initial Commit

---
+++ NotesEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NotesEditor
+//
+//  Created by Aaron on 1/19/16.
+//  Copyright © 2016 avontell.com. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
